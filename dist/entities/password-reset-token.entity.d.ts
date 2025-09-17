d30c9a236c74e39fbf648548e625b0e4ad3c664a
lchalk <leonard@readyskill.io>
2025-09-16 23:18:48 -0400

    chore(git): untrack dist directory (already in .gitignore)

--- dist/entities/password-reset-token.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.entity';
-export declare class PasswordResetToken {
-    id: number;
-    user_id: number;
-    token: string;
-    expires_at: Date;
-    created_at: Date;
-    user: User;
-}
--- dist/modules/auth-callbacks/dto/identity.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class IdentityDto {
-    user_id: number;
-    provider_uid: string;
-    provider_type: string;
-    auth_hash: string;
-}
