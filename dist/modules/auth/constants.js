52ed04da838999c75bcc4261ff28c2c7052708c3
lchalk <leonard@readyskill.io>
2025-09-16 22:39:53 -0400

    chore(git): stop tracking dist; ensure dist/ in .gitignore

--- dist/modules/auth/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtConstants = void 0;
-exports.jwtConstants = {
-    secret: process.env.JWT_SECRET || 'change_this_secret',
-    expiresIn: process.env.JWT_EXPIRES_IN || '1h',
-};
-//# sourceMappingURL=constants.js.map
